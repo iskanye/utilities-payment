1552c46300f81cf9088fde9f0c493008405cb88f
iskanye <iskanyeban@gmail.com>
2025-10-25 01:02:50 +0300

    refactor: deleted interfaces from models

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,26 +1,8 @@
 package models
 
-import "context"
-
 type User struct {
 	ID       int64
 	Email    string
 	PassHash []byte
 	Address  string
-}
-
-type UserSaver interface {
-	SaveUser(
-		ctx context.Context,
-		email string,
-		passHash []byte,
-		address string,
-	) (uid int64, err error)
-}
-
-type UserProvider interface {
-	User(
-		ctx context.Context,
-		email string,
-	) (User, error)
 }
